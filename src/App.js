10360a79078348442b8f5b2e19e0efaa68c75b89
Aleksandr62 <74791135+Aleksandr62@users.noreply.github.com>
2021-08-03 22:08:21 +0300

    les-2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { Message } from './components/Message.js';
-import './assets/style.sass'
+import { useState, useEffect } from 'react';
+import { InputText } from './components/InputText.js';
+import { MessageList } from './components/MessageList.js';
+import styles from './assets/style.module.sass';
 
-export function App() {
-  const [state,] = React.useState({msg: 'Привет мир!'});
+export const App = () => {
+  const [messageList, setMessageList] = useState([]);
+
+  function handleMsgChange({author, text}) {
+    if(!author) author = 'anonim';
+    setMessageList((messageList) => [...messageList, {author, text}]);
+  };
+
+  useEffect(() => {
+    const lastMessage = messageList[messageList.length - 1]
+
+    if (lastMessage?.author && lastMessage?.author !== "Bot") {
+      setTimeout(() => {
+        setMessageList((state) => [
+          ...state,
+          { text: `Hello ${lastMessage.author}`, author: "Bot" },
+        ])
+      }, 600)
+    }
+  }, [messageList])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Message msg={state.msg} />
-      </header>
+    <div className={styles.app}>
+        <MessageList styles={styles} messageList={messageList} />        
+        <InputText styles={styles} onMessageChange={handleMsgChange}/>
     </div>
   );
 }
