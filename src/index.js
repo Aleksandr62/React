21c6675bd7ffacae97bdeb21720fd709ea595caa
Aleksandr62 <74791135+Aleksandr62@users.noreply.github.com>
2021-08-13 13:30:11 +0300

    les4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import { ThemeProvider, createTheme } from "@material-ui/core";
+import { StrictMode } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Chat, Profile, PageNone, Home } from "./pages";
+import { Menu } from "./components";
+import Styles from "./styles.module.css";
+
+const theme = createTheme({
+  light: {
+    color: "#000",
+    backgroundColor: "#fff",
+    buttonColor: "red"
+  },
+  dark: {
+    color: "#fff",
+    backgroundColor: "#000",
+    buttonColor: "blue"
+  }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <header className={Styles.header}>
+          <Menu />
+        </header>
+        <Switch>
+          <Route path="/chat" component={() => <Chat />} />
+          <Route path="/profile" component={() => <Profile />} />
+          <Route exact path="/" component={() => <Home />} />
+          <Route path="*" component={() => <PageNone />} />
+        </Switch>
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>,
+  document.getElementById("root")
 );
